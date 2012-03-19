0125654d7f7c8fd0197c61781b83a493d80ae875
Ryan Northey <ryan@3ca.org.uk>
2012-03-19 18:48:00 +0000

    tidying up

--- bit/recipe/android_ndk/__init__.py
+++ bit/recipe/android_ndk/__init__.py
@@ -1,31 +1,36 @@
 import logging
 import os
-import shutil
 import pkg_resources
 import commands
 
+
 class Recipe:
+
     def __init__(self, buildout, name, options):
         self.options = options
         self.logger = logging.getLogger(name)
         self.name = name
 
     def _update(self):
-        install = pkg_resources.resource_filename(__name__, 'install_android_ndk')
+        install = pkg_resources.resource_filename(
+            __name__, 'install_android_ndk')
         path = os.getcwd()
-        target_install = os.path.join(path,'bin',self.name)
-        revision = 'r7'
-        part = os.path.join(path,'parts',self.name)
+        target_install = os.path.join(path, 'bin', self.name)
         if os.path.exists(target_install):
             os.unlink(target_install)
-        open(target_install,'w').write('#/bin/bash\nBUILDOUT=%s\nNDK=%s\nPARTNAME=%s\n' %(path,self.options['ndk'],self.name)+open(install).read())
-        commands.getoutput('chmod +x %s' %target_install)
-        #open(os.path.join('bin', self.name),'w').write('BUILDOUT=%s\nexport ANDROIDNDKVER=%s\nexport ANDROIDNDK=%s\nexport PATH=$ANDROIDNDK:$PATH'%(path,revision,part))
+        env_vars = {'BUILDOUT': path,
+                    'NDK': self.options['ndk'],
+                    'PARTNAME': self.name,
+                    }
+        bash = '#/bin/bash\n'
+        bash += '\n'.join(['%s=%s' % (k, v)
+                          for k, v in env_vars.items()])
+        open(target_install, 'w').write(bash + open(install).read())
+        commands.getoutput('chmod +x %s' % target_install)
 
     def install(self):
         self._update()
-        return ['bin/%s' %self.name]
+        return ['bin/%s' % self.name]
 
     def update(self):
         self._update()
-        #print commands.getoutput('./bin/%s ' %self.name)
